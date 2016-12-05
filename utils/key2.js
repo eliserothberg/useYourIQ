52aa16b7eea4b05353792f67bb2e68ca3bc7cb37
shaanobney <shaan.obney@gmail.com>
2016-12-04 16:30:13 -0800

    updated

--- utils/key2.js
+++ utils/key2.js
@@ -1,11 +1,11 @@
 var theList = { method: 'GET',
-  url: '-',
-  headers: 
-   { 
-     'authorization': '-',
+  url: 'https://api.salesforceiq.com/v2/lists/57114e10e4b0a3f93805ebc6',
+  headers:
+   {
+     'authorization': 'NTgwN2IyYmFlNGIwOWQzYjc1ZmMwMjU4OkUxaFZCc0ozQ1dPS0hnR3V6Rkx1NGNZTHkyNA==',
      'content-type': 'application/json'
    },
- 
+
   json: true
 };
 
