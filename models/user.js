51be90511baf4ba0f4906b313f0c83f3293028ce
eliserothberg <rothberg.elise@gmail.com>
2016-12-04 16:53:07 -0800

    added user table

--- models/user.js
+++ models/user.js
@@ -1,25 +1,18 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    username: DataTypes.STRING, 
+    username: DataTypes.STRING,
     email: DataTypes.STRING,
     password_hash: DataTypes.STRING,
     URL_hash: DataTypes.STRING,
-    authorization_hash:  DataTypes.STRING,
+    authorization_hash: DataTypes.STRING,
     active: DataTypes.BOOLEAN
   }, {
-
     classMethods: {
       associate: function(models) {
-        // User.hasMany(models.Event, {foreignKey: 'user_id'});
+        // associations can be defined here
       }
     }
-  })
-
+  });
   return User;
 };
