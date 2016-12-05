81756cb11c36b3388f2bb2538362fa07e5305558
eliserothberg <rothberg.elise@gmail.com>
2016-12-04 16:16:34 -0800

    added created date

--- models/users.js
+++ models/users.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
 
     classMethods: {
       associate: function(models) {
-        User.hasMany(models.Event, {foreignKey: 'user_id'});
+        // User.hasMany(models.Event, {foreignKey: 'user_id'});
       }
     }
   })
